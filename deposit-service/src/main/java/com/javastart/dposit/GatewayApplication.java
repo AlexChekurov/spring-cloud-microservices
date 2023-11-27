685f12241f9e1a8d6c8ca27bb3b3c5470eadd6cc
Alex <alexandr.chekurov@gmail.com>
2023-11-28 00:54:09 +0300

    Initial commit

---
+++ deposit-service/src/main/java/com/javastart/dposit/GatewayApplication.java
@@ -0,0 +1,7 @@
+package com.javastart.dposit;
+
+public class GatewayApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ gateway/src/main/java/com/javastart/dposit/DepositApplication.java
@@ -0,0 +1,7 @@
+package com.javastart.dposit;
+
+public class DepositApplication {
+    public static void main(String[] args) {
+
+    }
+}
